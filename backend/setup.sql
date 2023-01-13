dd72e66c46e866ba73775e1ce69e775d5437040b
cast <mc1@email.com>
2023-01-12 20:20:34 -0800

    First commit

---
+++ backend/setup.sql
@@ -0,0 +1,35 @@
+--Create our database table
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY,
+    summary VARCHAR(1024),
+    description TEXT,
+    is_active BOOLEAN DEFAULT 1
+);
+
+
+
+--- Populate our table with some dummy data
+
+INSERT INTO task (
+    summary,
+    description
+) VALUES (
+    "Take out the trash",
+    "Take the trash out to the dumpster by the driveway"
+);
+
+INSERT INTO task (
+    summary,
+    description
+) VALUES (
+    "Wash the car",
+    "Either take the car to the car wash or do it yourself"
+);
+
+INSERT INTO task (
+    summary,
+    description
+) VALUES (
+    "Prepare lunch",
+    "Make a wholesome meal"
+);
---
+++ frontend/run.sh
@@ -0,0 +1,7 @@
+
+
+export FLASK_APP=app/routes.py
+export FLASK_ENV=development
+
+flask run -p 5001
+
